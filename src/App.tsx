1127b61555dd748f0d15d4f8a8a177a5d769ab37
xOIBrandon <bhoward@xoi.io>
2021-09-18 13:49:22 -0500

    Add color tokens to tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="flex items-center w-12">
+        <div className="flex items-center w-full justify-between">
           <Block />
           <Block />
           <Block />
         </div>
 
-        <h3 className="">Hello world!</h3>
+        <h3 className="">Hello world!!!!!</h3>
       </header>
     </div>
   );
 }
 
-const Block = () => <div style={{width: 10, height: 10, borderColor: "lime", borderWidth: 1}} />
+const size = 100;
+const Block = () => (
+  <div
+    className="bg-aqua"
+    style={{ width: size, height: size, borderColor: "lime", borderWidth: 1 }}
+  />
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      magenta: { DEFAULT: "#FD408D", light: "#FFC5DC" },
+      coral: { DEFAULT: "#FF8472", light: "#FFDED9" },
+      aqua: { DEFAULT: "#60FFE2", light: "#C5FFF4" },
+      royal: { DEFAULT: "#1F6D99", light: "#7CCEFC" },
+    },
+    fontFamily: {
+      // sans: ["Graphik", "sans-serif"],
+      // serif: ["Merriweather", "serif"],
+      jubilate: ["Jubliat"],
+      base: ["Proxima Nova"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
