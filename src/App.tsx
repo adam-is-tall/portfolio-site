30e23d6a1949da4dee50cd822a7d2836b0a1726a
Adam Howard <howard.adamp@gmail.com>
2023-11-30 14:00:01 -0600

    type more

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ export default function App() {
 
         <div className="flex flex-col items-center p-md py-lg sm:p-lg md:p-xl lg:p-2xl text-center">
           {/* <ChatBubble className="mb-lg" frames={adamHeads2}>I'm fascinated by the creative process - turning ideas into reality. I'm fully aware that I'm not making products for my own artistic expression. I work to find the balance between what the business wants and what the users need.</ChatBubble> */}
-          <ChatBubble className="mb-lg" frames={adamHeads2}>I want to bring good design to engineering-led organizations.</ChatBubble>
+          <ChatBubble className="mb-lg" frames={adamHeads2}>I want to bring a better design discipline to engineering-led organizations. What does that mean? It's too much to fit in this bubble, but let's talk!</ChatBubble>
         </div>
         
         {/* <div className="flex flex-col items-center p-md py-lg sm:p-lg md:p-xl lg:p-2xl text-center relative">
